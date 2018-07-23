07c95ed9acdc51e3e33edfc779a7854d58a08080
Lucy Weatherley <lucyweath@msn.com>
2018-07-23 20:32:14 +0100

    Book moved to its own component + drop down value stored in state

---
+++ src/Book.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
+
+class Book extends Component {
+
+  state = {
+    id: '',
+    shelf: '',
+    title: '',
+    authors: '',
+    thumbnail: ''
+  }
+
+  // Gets book data from server
+  componentDidMount() {
+    BooksAPI.get(this.props.id).then(data => {
+      this.setState({
+        id: data.id,
+        shelf: data.shelf,
+        title: data.title,
+        authors: data.author,
+        thumbnail: data.imageLinks.thumbnail
+      })
+    })
+  }
+
+  // Updates state when shelf changed
+  handleChange = (event) => {
+    this.setState({
+      shelf: event.target.value
+    });
+  }
+
+  // Render book
+  render() {
+    return (
+      <li id={this.state.id}>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{backgroundImage: 'url('+this.state.thumbnail+')'}}>
+            </div>
+            <div className="book-shelf-changer">
+              <select value={this.state.shelf} onChange={this.handleChange}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{this.state.title}</div>
+          <div className="book-authors">{this.state.author}</div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Book
--- src/WantToRead.js
+++ src/WantToRead.js
@@ -1,40 +1,22 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class WantToRead extends Component {
-    render() {
-        return (
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {this.props.books.map((book, i) => (
-                    <li key={book.title+i}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div
-                            className="book-cover"
-                            style={{backgroundImage: 'url('+book.imageLinks.thumbnail+')'}}>
-                          </div>
-                          <div className="book-shelf-changer">
-                            <select>
-                              <option value="move" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.author}</div>
-                      </div>
-                    </li>
-                  ))}
-                </ol>
-              </div>
-            </div>
-        )
-    }
+
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Want to Read</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book) => (
+              <Book id={book.id} />
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default WantToRead
